9e8c131217d8cdf9bc4fee317e1fea4f7888dec1
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-03 21:52:04 +0000

    import CharacterList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { allCharacters } from "../data/data";
 import "./App.css";
+import CharachterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className=" flex items-center justify-between w-full gap-8">
+        <CharachterList allCharacters={allCharacters} />
+        {/* <CharacterDetail /> */}
+      </div>
     </div>
   );
 }
