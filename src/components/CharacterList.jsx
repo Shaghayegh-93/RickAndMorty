d5e329a0905c49fcfd04d490f633995ed7ee308d
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-05 20:02:32 +0000

    responsive style

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CharacterList = ({ allCharacters }) => {
   return (
-    <div>
+    <div className="">
       {allCharacters.map((character) => (
         <Character key={character.id} character={character} />
       ))}
@@ -15,17 +15,17 @@ export default CharacterList;
 
 function Character({ character }) {
   return (
-    <div className="grid grid-cols-[4rem,1fr,2rem,2rem] grid-rows-2 gap-x-4 bg-slate800 rounded-lg p-3 cursor-pointer transition-all duration-[0.2s] ease-out hover:bg-slate700 mb-6 last:mb-0">
+    <div className="grid grid-cols-[4rem,1fr,2rem,2rem] md:grid-cols-[4rem,1fr,2rem,2rem] grid-rows-2 gap-x-4 bg-slate800 rounded-lg p-3 cursor-pointer transition-all duration-[0.2s] ease-out hover:bg-slate700 mb-6 last:mb-0">
       <img
         src={character.image}
         alt=""
         className="col-start-1 col-end-3 row-start-1 row-end-3  w-16 h-16 rounded-2xl"
       />
-      <h3 className="col-start-2 col-end-3 row-start-1 row-end-2">
+      <h3 className="col-start-2 col-end-5 md:col-end-3 row-start-1 row-end-2 text-white">
         <span>{character.gender === "Male" ? "🙍‍♂️" : "🙎‍♀️"}</span>
         <span>{character.name}</span>
       </h3>
-      <div className=" col-start-2 col-end-3 row-start-2 row-end-3 self-center text-slate200">
+      <div className=" col-start-2 col-end-5  md:col-end-3 row-start-2 row-end-3 self-center text-slate200">
         <span
           className={`inline-block w-3 h-3 rounded-[50%] bg-green600 mr-1 
           
@@ -35,7 +35,7 @@ function Character({ character }) {
         <span>{character.status}</span> - &nbsp;
         <span>{character.species}</span>
       </div>
-      <button className="col-start-5 col-end-5 row-start-1 row-end-3 self-center  col-span-2  text-rose500  ">
+      <button className=" ml-3 md:ml-0 col-start-4 col-end-4 md:col-start-5 md:col-end-5 row-start-1 row-end-3 self-center  col-span-2  text-rose500  ">
         <EyeIcon className="w-6 h-6  " />
       </button>
     </div>
