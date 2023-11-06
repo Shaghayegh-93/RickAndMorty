4911a5a431456a36dbeb5a852bfebda871828177
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-06 21:32:45 +0000

    add useEffect and fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import { useEffect, useState } from "react";
-import { allCharacters } from "../data/data";
+// import { allCharacters } from "../data/data";
 import "./App.css";
 import CharacterDetail from "./components/CharacterDetail";
 import CharachterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  // const [allCharacters, setallCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [allCharacters, setallCharacters] = useState([]);
+  const [serach, setSearch] = useState("");
 
-  useEffect(()=>{
-    try {
-      
-    } catch (error) {
-      
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character/?name=${serach}`
+        );
+        setallCharacters(data.results.slice(0, 5));
+      } catch (error) {
+        toast.error(error.response.data.error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (serach.length < 3) {
+      setallCharacters([]);
+      return;
     }
-  },[])
+    fetchData();
+  }, [serach]);
   return (
     <div className="">
-      <Navbar />
+      <Toaster />
+      <Navbar
+        numOfList={allCharacters?.length}
+        serach={serach}
+        setSearch={setSearch}
+      />
       <div className=" flex flex-col md:flex-row justify-between w-full gap-10">
-        <CharachterList allCharacters={allCharacters} />
+        <CharachterList allCharacters={allCharacters} isLoading={isLoading} />
         <CharacterDetail />
       </div>
     </div>
