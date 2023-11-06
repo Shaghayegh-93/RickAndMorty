2cea1ddf06a37e9f692e08d36db04bbb187e8ff6
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-06 21:00:15 +0000

    add value and onchange to input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeartIcon } from "@heroicons/react/24/outline";
 
-const Navbar = () => {
+const Navbar = ({ numOfList,serach,setSearch }) => {
   return (
     <nav className="flex items-center justify-between  bg-slate700 p-4 rounded-2xl mb-4 text-white">
       <h1 className="text-slate300 font-bold">Logo</h1>
@@ -9,8 +9,10 @@ const Navbar = () => {
         className="py-3 px-4 rounded-lg text-slate100 bg-slate500 text-base hidden md:inline-block"
         type="text"
         placeholder="Serach..."
+        value={serach}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <p className="text-slate400">found x result </p>
+      <p className="text-slate400">found {numOfList} result </p>
       <button className="relative  text-rose500">
         <HeartIcon className="w-8 h-8" />
         <span className="absolute text-xs top-0 -right-1 h-4 leading-4 text-center  bg-rose500 text-white rounded-[50%]  py-[1px] px-[4px]  flex items-center justify-center">
