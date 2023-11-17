9f1919de66d76608d206c72947260f0449ab1420
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-17 22:48:11 +0000

    improve the logic based on our project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,21 @@
 import { XCircleIcon } from "@heroicons/react/24/outline";
-import React from "react";
 
-const Modal = () => {
+const Modal = ({ children, open, onOpen }) => {
+  if (!open) return null;
   return (
-    <div>
-      <div className="w-screen h-screen fixed inset-0 bg-[rgba(63, 69, 76, 0.8)]">
-        <div className="w-[98%] min-h-[250px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-900 p-4 rounded-2xl shadow-[1px 1px 50px slate-600]">
-          <div className="flex items-center justify-between mb-4 pb-2 border-b-[1px] border-slate-600">
-            <h2 className="text-slate-200">title</h2>
-            <button>
-              <XCircleIcon />
-            </button>
-          </div>
+    <div className="">
+      <div
+        className="w-screen h-screen fixed inset-0 bg-backdrop "
+        onClick={() => onOpen(false)}
+      ></div>
+      <div className=" w-[50%] min-h-[250px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-900 p-4 rounded-2xl shadow-modalShadow">
+        <div className="flex items-center justify-between mb-4 pb-2 border-b-[1px] border-slate-600">
+          <h2 className="text-slate-200">title</h2>
+          <button onClick={() => onOpen(false)}>
+            <XCircleIcon className="w-8 h-8 text-red-500" />
+          </button>
         </div>
+        {children}
       </div>
     </div>
   );
