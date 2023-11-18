6aa1940aa3694a314897682e3169238b52b3a2e0
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-18 18:12:48 +0000

    add hooks folder

---
+++ src/hooks/useCharacters.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function useCharacters(url, serach) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [allCharacters, setallCharacters] = useState([]);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}=${serach}`, { signal });
+        setallCharacters(data.results.slice(0, 5));
+      } catch (error) {
+        if (!axios.isCancel()) {
+          setallCharacters([]);
+          toast.error(error.response.data.error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+    return () => {
+      controller.abort();
+    };
+  }, [serach]);
+  return { isLoading, allCharacters };
+}
