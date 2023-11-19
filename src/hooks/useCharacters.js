6545426a4e2d310c97b57c608fb3adfa68f8e0ee
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-19 22:39:47 +0000

    import useEffect

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function useCharacters(url, serach) {
