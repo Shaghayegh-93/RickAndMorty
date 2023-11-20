e246f371a6172211354b9861b5b66317cd0151e7
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-11-20 22:19:08 +0000

    improve style of component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { LoaderIcon } from "react-hot-toast";
 
 const Loader = () => {
   return (
-    <div className="text-white">
+    <div className="text-slate-300 flex items-center gap-4 ">
       <p>Data is Loading...</p>
+      <LoaderIcon className="w-5 h-5" />
     </div>
   );
 };
